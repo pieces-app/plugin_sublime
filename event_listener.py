3b57a0eee3904c6ef661762ed9872e28e07c06c0
BishoyHanyRaafat <bishoy@pieces.app>
2024-04-25 02:16:56 +0200

    remove the print statment

--- event_listener.py
+++ event_listener.py
@@ -8,7 +8,6 @@ class PiecesEventListener(sublime_plugin.EventListener):
     def on_window_command(self, view, command_name, args):
         # List of commands to check
         commands_to_check = ['pieces_list_assets','pieces_handle_markdown']
-        print(command_name)
         if command_name in commands_to_check and not self.check_condition():
             sublime.message_dialog("The pieces os server is not running")
             # Cancel the command by replacing it with a no-op
