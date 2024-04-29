b8f3ac9e6a187fce132b610b1c5bc74f9064b5bc
Bishoy-at-pieces <bishoy@pieces.app>
2024-04-29 13:04:41 +0300

    Add ok_cancel dialog on closing an asset #24

--- event_listener.py
+++ event_listener.py
@@ -2,25 +2,31 @@ import sublime
 import sublime_plugin
 from pieces.api import get_health
 
+from pieces.assets.markdown_handler import PiecesHandleMarkdownCommand
+
 
 
 class PiecesEventListener(sublime_plugin.EventListener):
-    def on_window_command(self, view, command_name, args):
-        
-        # List of commands to check
-        commands_to_check = ['pieces_list_assets',"pieces_delete_asset"]
-        self.check(command_name,commands_to_check)
-        
-    def on_text_command(self,view,command_name,args):
-        commands_to_check = ['pieces_create_asset',"pieces_ask_question"]
-        self.check(command_name,commands_to_check)
+	def on_window_command(self, view, command_name, args):
+		
+		# List of commands to check
+		commands_to_check = ['pieces_list_assets',"pieces_delete_asset"]
+		self.check(command_name,commands_to_check)
+		
+	def on_text_command(self,view,command_name,args):
+		commands_to_check = ['pieces_create_asset',"pieces_ask_question"]
+		self.check(command_name,commands_to_check)
 
-    def check(self,command_name,commands_to_check):
-        if command_name in commands_to_check:
-            if not get_health():
-                sublime.message_dialog("The pieces os server is not running")
-                return False
-        return None
-        
-
-
+	def check(self,command_name,commands_to_check):
+		if command_name in commands_to_check:
+			if not get_health():
+				sublime.message_dialog("The pieces os server is not running")
+				return False
+		return None
+	def on_pre_close(self,view):
+		asset_id = PiecesHandleMarkdownCommand.views_to_handle.get(view.id())
+		if asset_id:
+			if sublime.ok_cancel_dialog("Do you want to this snippet to pieces?", ok_title='Save', title='Save snippet'):
+				view.window().run_command("pieces_handle_markdown",{"mode": "save"})
+			else:
+				view.window().run_command("pieces_list_assets",{"pieces_asset_id":asset_id})
