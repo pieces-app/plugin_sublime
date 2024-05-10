01deeb3e8eb08769f7e8a865f15ec1f30f54ae6a
Bishoy-at-pieces <bishoy@pieces.app>
2024-05-10 18:29:41 +0300

    add allocation on login

--- auth/auth_command.py
+++ auth/auth_command.py
@@ -1,12 +1,15 @@
 import sublime_plugin
 
 from pieces.settings import PiecesSettings
+from .auth_user import AuthUser
+
 from pieces_os_client import OSApi
 
 
 class PiecesLoginCommand(sublime_plugin.WindowCommand):
 	def run(self):
 		OSApi(PiecesSettings.api_client).sign_into_os(async_req=True)
+		AuthUser.connect = True # To connect to the cloud after logining in
 
 
 class PiecesLogoutCommand(sublime_plugin.WindowCommand):
--- auth/auth_user.py
+++ auth/auth_user.py
@@ -1,17 +1,23 @@
-from pieces_os_client import UserProfile
-
+from pieces_os_client import UserProfile,AllocationsApi
+from pieces.settings import PiecesSettings
 import sublime
 
 class AuthUser:
 	output_panel = None
 	phantom_set = None
+	connect = False # should we connect to the allocation server
 
 	@classmethod
 	def on_user_callback(cls,user:UserProfile=None):
+		
 		if not user:
 			cls.login_page()
 		else:
-			cls.logout_page(user.email,user.name,user.allocation)
+			cls.logout_page(user.email,user.name,user.allocation,cls.connect)
+			if cls.connect:
+				AllocationsApi(PiecesSettings.api_client).allocations_connect_new_cloud(user)
+				cls.connect = False # Rest the connect variable
+
 
 	@classmethod
 	def login_page(cls):
@@ -21,8 +27,7 @@ class AuthUser:
 		cls.phantom_set.update([phantom])
 
 	@classmethod
-	def logout_page(cls,email,username,allocation):
-
+	def logout_page(cls,email,username,allocation,connect):
 		if allocation:
 			allocation_html = f"<p>Cloud Status: <span style=color:green>•</span> Connected</p>"
 			try:
@@ -31,7 +36,11 @@ class AuthUser:
 			except AttributeError:
 				pass
 		else:
-			allocation_html = "<p>Cloud Status: <span style=color:red>•</span> Disconnected</p>"
+			if connect:
+				allocation_html = "<p>Cloud Status: <span style=color:yellow>•</span> Connecting</p>"
+			else:
+				allocation_html = "<p>Cloud Status: <span style=color:red>•</span> Disconnected</p>"
+		
 		cls.phantom_set.update([])
 		phantom_content = f"<p>Username: {username}</p><p>Email: {email}</p>{allocation_html}<a href='subl:pieces_logout'>Logout</a>"
 		phantom = sublime.Phantom(sublime.Region(0, 500), phantom_content, sublime.LAYOUT_INLINE)
