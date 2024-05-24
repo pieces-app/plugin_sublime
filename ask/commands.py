6437cc6cda7aaee85c7cd106b1251e36ade95227
Bishoy-at-pieces <bishoy@pieces.app>
2024-05-24 18:22:13 +0300

    remove extra dots

--- ask/commands.py
+++ ask/commands.py
@@ -52,7 +52,7 @@ class PiecesAskQuestionCommand(sublime_plugin.TextCommand):
 
 
 	def on_done(self,description):
-		self.description = description....
+		self.description = description
 		sublime.set_timeout_async(self.on_done_async,0)
 
 	def on_done_async(self):
