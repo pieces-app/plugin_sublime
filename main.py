03b6c69de3646c1297fa38a76c85e68e2d8258a7
Bishoy-at-pieces <bishoy@pieces.app>
2024-09-05 16:41:17 +0300

    refactor main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from . import __version__
-from .api import open_pieces_os,print_version_details,version_check
 from .settings import PiecesSettings
 from .copilot.ask_command import copilot
-from .health_ws import HealthWS
 import sublime
+from ._pieces_lib.pieces_os_client.wrapper.version_compatibility import VersionChecker,UpdateEnum
+from ._pieces_lib.pieces_os_client.wrapper.websockets import BaseWebsocket,AuthWS,HealthWS
 
 # load the commands
 from .assets import *
@@ -12,63 +12,64 @@ from .auth import *
 from .search import *
 from .misc import *
 from .copilot import *
-from .base_websocket import BaseWebsocket
+
+PIECES_OS_MIN_VERSION = "10.1.5"  # Minimum version (10.0.3)
+PIECES_OS_MAX_VERSION = "11.0.0" # Maximum version (11.0.0)
 
 
-
-def startup(settings_model):
-	pieces_version = None
-	if PiecesSettings.get_settings().get("auto_start_pieces_os"):
-		pieces_version = open_pieces_os()
-
-
-	if not pieces_version:
-		print("Couldn't start pieces OS\nPlease run Pieces OS and restart the editor to ensure everything is running properly")
+def startup():
+	pieces_os_version = PiecesSettings.api_client.version
+	version_result = VersionChecker(PIECES_OS_MIN_VERSION,PIECES_OS_MAX_VERSION,pieces_os_version).version_check()
+	if version_result.compatible:
+		print(f"Pieces OS version: {pieces_os_version}\nPlugin version: {__version__}")
+		PiecesSettings.models_init(PiecesSettings.get_settings().get('model')) # Intilize the models
+	else: 
+		if version_result.update == UpdateEnum.PiecesOS:
+			update = "Pieces OS"
+		else:
+			update = "Pieces Sublime Plugin"
+		sublime.message_dialog(f"{update} is outdated. Can you please update {update}")
+		PiecesSettings.is_loaded = False
+		BaseWebsocket.close_all()
 		return
-	else:
-		if version_check()[0]:
-			PiecesSettings.is_loaded = True
-			PiecesSettings.compatible = True
-			PiecesSettings.get_application()
-			print_version_details(pieces_version, __version__)
-			PiecesSettings.models_init(settings_model) # Intilize the models
-		else: return
-
-	
-	# WEBSOCKETS:
-	# Assets Identifiers Websocket
-	AssetsIdentifiersWS(AssetSnapshot.streamed_identifiers_callback).start() # Load the assets ws at the startup
 	
 	# User Weboscket
 	PiecesSettings.create_auth_output_panel()
-
-	AuthWebsocket(AuthUser.on_user_callback).start() # Load the stream user websocket
-
-	# Conversation Websocket
-	ConversationWS(ConversationsSnapshot.streamed_identifiers_callback).start()
-
-	# Health websocket
-	HealthWS().start()
+	auth_ws = AuthWS.get_instance()
+	auth_ws.on_message_callback = AuthUser.on_user_callback
 
 	# Lunch Onboarding if it is the first time
 	if not PiecesOnboardingCommand.get_onboarding_settings().get("lunch_onboarding",False):
 		sublime.active_window().run_command("pieces_onboarding")
 		PiecesOnboardingCommand.add_onboarding_settings(lunch_onboarding=True)
 
-def plugin_loaded():
-	global settings # Set it to global to use 
+def on_message(message):
+	if message == "OK":
+		PiecesSettings.is_loaded = True
+	else:
+		PiecesSettings.is_loaded = False
+		print("Please make sure Pieces OS is running\n")
 
-	settings = sublime.load_settings('Pieces.sublime-settings')
+def on_close(ws):
+	PiecesSettings.is_loaded = False
+
+def plugin_loaded():
+	settings = PiecesSettings.get_settings()
 	host = settings.get("host")
-	model = settings.get('model')
-	settings.add_on_change("PIECES_SETTINGS",PiecesSettings.on_settings_change)
-	PiecesSettings.host_init(host) # Intilize the hosts url
 	
+	PiecesSettings.host_init(host) # Intilize the hosts url
 	# callbacks needed onchange settings
 	PiecesSettings.on_model_change_callbacks.append(copilot.update_status_bar)
+	health = PiecesSettings.api_client.health
+	if PiecesSettings.get_settings().get("auto_start_pieces_os") and not health:
+			health = PiecesSettings.api_client.open_pieces_os()
 
-	sublime.set_timeout_async(lambda : startup(model) ,0)
-	
+	if health:
+		HealthWS(PiecesSettings.api_client, on_message, startup, on_close).start()
+	else:
+		print("Couldn't start pieces OS\nPlease run Pieces OS and restart the editor to ensure everything is running properly")
+		BaseWebsocket.close_all()
+
 
 def plugin_unloaded():
 	BaseWebsocket.close_all()
