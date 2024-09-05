db15208bac770d09a890ff3deae30c207c58893e
Bishoy-at-pieces <bishoy@pieces.app>
2024-09-05 16:41:40 +0300

    refactor snippets details autocomplete

--- snippet_details_autocomplete.py
+++ snippet_details_autocomplete.py
@@ -1,13 +1,13 @@
+from ._pieces_lib.pieces_os_client.wrapper.basic_identifier.asset import BasicAsset
 import sublime_plugin
 import mdpopups
-from .assets.assets_snapshot import AssetSnapshot
-from .assets.list_assets import PiecesAssetIdInputHandler
+
 class PiecesShowCompletionDetailsCommand(sublime_plugin.TextCommand):
 	def run(self,edit,asset_id):
-		asset_wrapper = AssetSnapshot(asset_id)
-		lang = asset_wrapper.original_classification_specific().value
-		content = asset_wrapper.get_asset_raw()
-		details = PiecesAssetIdInputHandler.get_annotation(asset_wrapper.asset)
+		asset_wrapper = BasicAsset(asset_id)
+		lang = asset_wrapper.classification.value if asset_wrapper.classification else "txt"
+		content = asset_wrapper.raw_content
+		details = asset_wrapper.description
 		mdpopups.show_popup(
 			self.view,
 			f"{details.text if details else ''}\n```{lang}\n{content}\n```",max_width=900,
