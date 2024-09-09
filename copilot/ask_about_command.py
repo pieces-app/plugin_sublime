14ebf21ca20708af2374c06e1dd703f4cac60374
Bishoy-at-pieces <bishoy@pieces.app>
2024-09-10 00:05:28 +0300

    fix context issue

--- copilot/ask_about_command.py
+++ copilot/ask_about_command.py
@@ -6,27 +6,27 @@ from ..settings import PiecesSettings
 
 class PiecesAskStreamAboutCommand(sublime_plugin.TextCommand):
 	def run(self,edit,type,pieces_query):
-		self.context = PiecesSettings.api_client.copilot.context
+		copilot.render_conversation(None) # Create conversation if not already
 		self.before_query = ""
 		if type == "file":
 			path = self.view.file_name()
 			if not path: return 
-			self.context.paths.append(path)
+			PiecesSettings.api_client.copilot.context.paths.append(path)
+
 		elif type == "folder":
 			window = self.view.window()
 			if not window: return
 			paths = window.folders()
 			if not paths: return
-			[self.context.paths.append(path) for path in paths]
+			[PiecesSettings.api_client.copilot.context.paths.append(path) for path in paths]
 
 		elif type == "section":
 			# Get the all the selected text
 			data = "\n".join([self.view.substr(selection) for selection in self.view.sel()])
 			if not data:
 				return sublime.error_message("Please select a text")
-			self.context.raw_assets.append(data)
+			PiecesSettings.api_client.copilot.context.raw_assets.append(data)
 
-		copilot.render_conversation(None) # Create conversation if not already
 		if self.before_query: 
 			pieces_query = self.before_query + pieces_query
 		copilot.add_query(pieces_query) # Add the query
