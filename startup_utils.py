74e4358862ed61651d2c2ea932ce6833ee634b8c
Bishoy Hany <167128903+bishoy-at-pieces@users.noreply.github.com>
2025-04-10 17:29:13 +0200

    Update startup_utils.py

--- startup_utils.py
+++ startup_utils.py
@@ -4,8 +4,8 @@ from .settings import PiecesSettings
 import sublime
 from functools import wraps
 
-PIECES_OS_MIN_VERSION = "12.0.0"  # Minium version (12.0.0)
-PIECES_OS_MAX_VERSION = "13.0.0" # Maxium version (13.0.0)
+PIECES_OS_MIN_VERSION = "11.3.0"  # Minium version (11.3.0)
+PIECES_OS_MAX_VERSION = "12.0.0" # Maxium version (12.0.0)
 
 compatiablity_result = None
 
